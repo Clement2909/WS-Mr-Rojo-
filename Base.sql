fe5ea904235ffc6efb970d0b1e671e19fef6d861
Clement2909 <99732732+Clement2909@users.noreply.github.com>
2024-01-11 21:20:06 +0300

    Add files via upload
    Premier Bdd(modifiable)

---
+++ Base.sql
@@ -0,0 +1,86 @@
+CREATE DATABASE wsrojo;
+\c wsrojo;
+
+CREATE TABLE users (
+id serial PRIMARY KEY,
+login   varchar(30) unique  not null,
+pwd     varchar(30) not null
+);
+
+CREATE TABLE category (
+id serial PRIMARY KEY ,
+category varchar (30) not null
+);
+
+CREATE TABLE color(
+id serial PRIMARY KEY,
+color varchar(30) not null 
+); 
+
+CREATE TABLE marque(
+id serial PRIMARY KEY,
+marque varchar(30) not null
+);
+
+CREATE TABLE car(
+id serial PRIMARY KEY,
+matricule varchar(30) not null,
+id_marque int not null
+id_category int not null,
+id_color int not null,
+FOREIGN KEY(id_marque) REFERENCES marque(id),
+FOREIGN KEY(id_category) REFERENCES category(id),
+FOREIGN KEY(id_color) REFERENCES color(id)
+);
+
+CREATE TABLE annonce(
+id serial PRIMARY KEY,
+id_car int not null,
+datepub date  not null,
+prix  float not null ,
+id_user int not null,
+FOREIGN KEY(id_car)REFERENCES car(id),
+FOREIGN KEY (id_user) REFERENCES user(id)
+);
+
+
+CREATE TABLE magasin(
+id serial PRIMARY KEY,
+status varchar(20) not null,
+id_user int not null,
+id_car int not null,
+FOREIGN KEY(id_user) REFERENCES user(id),
+FOREIGN KEY (id_car) REFERENCES car(id)
+);
+
+
+CREATE TABLE commission(
+id serial PRIMARY KEY,
+id_category int not null,
+prixpourcentage int not null,
+FOREIGN KEY(id_category) REFERENCES category(id)
+);
+
+
+CREATE TABLE vendu(
+id serial PRIMARY KEY,
+id_annonce int not null,
+id_commission int not null,
+datevente date not null,
+FOREIGN KEY(id_annonce) REFERENCES annonce(id),
+FOREIGN KEY(id_commission) REFERENCES commission(id)
+);
+
+CREATE TABLE achat(
+id serial PRIMARY KEY,
+dateachat date not null,
+id_car int not null,
+id_user int not null,
+FOREIGN KEY(id_car) REFERENCES car(id),
+FOREIGN KEY(id_user) REFERENCES user(id)
+);
+
+
+
+
+
---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users VALUES
+('Toavina','Toavina'),('Clement','Clement'),('Mickael','Mickael');
+
+INSERT INTO color VALUES
+('rouge'),('noir'),('bleu'),('gris'),('vert'),('jaune');
+
+INSERT INTO category VALUES 
+('SUV'),('berline'),('coupe'),('decapotable'),('break'),('monospace'),('citadine'),('crossover'),('compacte'),('sportive');
+
+INSERT INTO marque VALUES
+('Toyota'),('Ford'),('BMW'),('Mercedes-Benz'),('Honda'),('Volkswagen'),('Tesla'),('Nissan'),('Audi'),('Chevrolet');
